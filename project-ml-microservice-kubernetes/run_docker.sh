a44e424e02c9fbe7828d3b2e7c87ba9b9d65338f
Peter Dev <4706435+makafanpeter@users.noreply.github.com>
2023-02-19 14:54:05 +0000

    Worked on task

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-
-# Step 2: 
+docker build -t makafanpeter/house-price-prediction-api .
+# Step 2:
 # List docker images
-
-# Step 3: 
+docker image ls
+# Step 3:
 # Run flask app
+docker run -p 8000:80 --name house-price-prediction-api makafanpeter/house-price-prediction-api
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=makafanpeter/house-price-prediction-api
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $dockerpath:v1.0 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
